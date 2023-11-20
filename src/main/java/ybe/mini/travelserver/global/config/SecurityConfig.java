327d892747d5a68358ed887189ef893b3712e6e4
minjung <ypd06021@naver.com>
2023-11-20 19:25:23 +0900

    Feat: cors 설정 적용

--- src/main/java/ybe/mini/travelserver/global/config/SecurityConfig.java
+++ src/main/java/ybe/mini/travelserver/global/config/SecurityConfig.java
@@ -7,22 +7,27 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SecurityConfig {
-
+public class SecurityConfig implements WebMvcConfigurer  {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.httpBasic(AbstractHttpConfigurer::disable);
         http.csrf(AbstractHttpConfigurer::disable);
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
-
         http.authorizeHttpRequests(httpRequests -> httpRequests
                 .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
                 .anyRequest().authenticated()
         );
-
         return http.build();
     }
 }
