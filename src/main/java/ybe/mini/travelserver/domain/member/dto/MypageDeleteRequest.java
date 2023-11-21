413c567ea700300be951a2ddd54f3dd856d07f29
okjaeook <okjaeook98@gmail.com>
2023-11-21 16:13:59 +0900

    Refactor: dto 반환 json 통일
    snake 적용

--- src/main/java/ybe/mini/travelserver/domain/member/dto/MypageDeleteRequest.java
+++ src/main/java/ybe/mini/travelserver/domain/member/dto/MypageDeleteRequest.java
@@ -1,14 +1,17 @@
 package ybe.mini.travelserver.domain.member.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import ybe.mini.travelserver.domain.member.entity.Member;
 
 import java.io.Serializable;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DeleteRequest(
+public record MypageDeleteRequest(
         @Email
         String email,
         @NotBlank
--- src/main/java/ybe/mini/travelserver/domain/member/dto/MypageDeleteResponse.java
+++ src/main/java/ybe/mini/travelserver/domain/member/dto/MypageDeleteResponse.java
@@ -1,21 +1,24 @@
 package ybe.mini.travelserver.domain.member.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import ybe.mini.travelserver.domain.member.entity.Member;
 
 import java.io.Serializable;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DeleteResponse(
+public record MypageDeleteResponse(
         @Email
         String email,
         @NotBlank
         String password
 ) implements Serializable {
-    public static DeleteResponse fromEntity(Member member) {
-        return new DeleteResponse(
+    public static MypageDeleteResponse fromEntity(Member member) {
+        return new MypageDeleteResponse(
                 member.getEmail(),
                 member.getPassword()
         );
