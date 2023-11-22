62c9825c9851c641d61b12f915e687a56dc61c43
유현 <yh7119@gmail.com>
2023-11-22 15:50:12 +0900

    Fix: accommodation 부분 URL 수정

--- src/main/java/ybe/mini/travelserver/domain/accommodation/controller/AccommodationController.java
+++ src/main/java/ybe/mini/travelserver/domain/accommodation/controller/AccommodationController.java
@@ -17,10 +17,10 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/accomodations")
+@RequestMapping("/accommodations")
 public class AccommodationController {
 
-    @GetMapping("")
+    @GetMapping("/keyword")
     public ResponseDto<List<AccommodationGetResponse>> getAccommodationByKeyword(
             @RequestParam String keyword
     ) {
@@ -71,9 +71,10 @@ public class AccommodationController {
         return new ResponseDto<>(HttpStatus.OK.value(), responseList);
     }
 
-    @GetMapping("")
+    @GetMapping("/city")
     public ResponseDto<List<AccommodationGetResponse>> getAccommodationByCityAndDistrict(
-            @RequestParam String city, @RequestParam String district
+            @RequestParam String city,
+            @RequestParam String district
     ) {
         Location location = Location.builder()
                 .address("강원특별자치도 강릉시 창해로 307 ")
