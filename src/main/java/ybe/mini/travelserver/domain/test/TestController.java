8713a0c1889008b44ad9c3024045bdf2f05bcdb9
minjung <ypd06021@naver.com>
2023-11-22 20:44:50 +0900

    Feat: request body 그대로 반환하는 컨트롤러 추가

--- src/main/java/ybe/mini/travelserver/domain/test/TestController.java
+++ src/main/java/ybe/mini/travelserver/domain/test/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 import ybe.mini.travelserver.global.common.ResponseDto;
 import ybe.mini.travelserver.global.security.PrincipalDetails;
 
@@ -16,8 +17,16 @@ import ybe.mini.travelserver.global.security.PrincipalDetails;
 @RequiredArgsConstructor
 public class TestController {
 
+    // "test" 응답
     @GetMapping()
     public ResponseDto<String> test (@AuthenticationPrincipal PrincipalDetails principalDetails) {
         return new ResponseDto<>(HttpStatus.OK.value(), "test");
     }
+
+    // 받은 body를 그대로 응답으로 반환
+    @GetMapping("/echo")
+    public ResponseDto<Object> echoRequestBody(@RequestBody Object requestBody) {
+        return new ResponseDto<>(HttpStatus.OK.value(), requestBody);
+    }
+
 }
