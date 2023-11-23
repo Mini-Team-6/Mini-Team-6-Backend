cae145fe15580373427e05c284cfae92837d8785
유현 <yh7119@gmail.com>
2023-11-23 22:34:59 +0900

    Test: Room 도메인 관련 테스트 코드 작성

---
+++ src/test/java/ybe/mini/travelserver/domain/room/controller/RoomControllerTest.java
@@ -0,0 +1,64 @@
+package ybe.mini.travelserver.domain.room.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import ybe.mini.travelserver.domain.room.DummyObjectForController;
+import ybe.mini.travelserver.domain.room.dto.RoomGetResponse;
+import ybe.mini.travelserver.domain.room.service.RoomService;
+import ybe.mini.travelserver.global.common.ResponseDto;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+
+@ExtendWith(MockitoExtension.class)
+class RoomControllerTest extends DummyObjectForController {
+    @Mock
+    RoomService roomService;
+    @InjectMocks
+    RoomController roomController;
+
+    @DisplayName("숙소에 따른 객실 전체 조회")
+    @Test
+    void bringRooms_success() {
+        //given
+        Long accommodationId = 1L;
+        List<RoomGetResponse> rooms = List.of(dummyRoomGetResponse());
+        given(roomService.bringRooms(accommodationId)).willReturn(rooms);
+
+        //when
+        ResponseDto<List<RoomGetResponse>> response = roomController.bringRooms(accommodationId);
+
+        //then
+        assertEquals(HttpStatus.OK.value(), response.status());
+        assertEquals(rooms, response.data());
+        then(roomService).should().bringRooms(accommodationId);
+    }
+
+    @DisplayName("객실 상세 조회")
+    @Test
+    void bringRoom() {
+        //given
+        Long roomId = 1L;
+        RoomGetResponse room = dummyRoomGetResponse();
+        given(roomService.bringRoom(roomId)).willReturn(room);
+
+        //when
+        ResponseDto<RoomGetResponse> response = roomController.bringRoom(roomId);
+
+        //then
+        assertEquals(HttpStatus.OK.value(), response.status());
+        assertEquals(room, response.data());
+        then(roomService).should().bringRoom(roomId);
+    }
+
+}
+
