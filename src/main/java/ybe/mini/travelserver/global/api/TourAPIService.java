539ad0f106c0c26a31663df58c5d7b80807dbb16
okjaeook <okjaeook98@gmail.com>
2023-11-25 01:15:19 +0900

    Feat: 숙박, 객실 조회 from Tour API
    bringAccommodation 에서
    api 2회 요청됨(간단 숙박 + 상세 숙박)
    
    프론트와 상의 필요

--- src/main/java/ybe/mini/travelserver/global/api/TourAPIService.java
+++ src/main/java/ybe/mini/travelserver/global/api/TourAPIService.java
@@ -1,101 +1,111 @@
 package ybe.mini.travelserver.global.api;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ybe.mini.travelserver.domain.accommodation.Location;
 import ybe.mini.travelserver.domain.accommodation.entity.Accommodation;
-import ybe.mini.travelserver.domain.accommodation.repository.AccommodationRepository;
 import ybe.mini.travelserver.domain.room.entity.Room;
-import ybe.mini.travelserver.domain.room.repository.RoomRepository;
+import ybe.mini.travelserver.global.api.dto.DetailInfoResponse;
+import ybe.mini.travelserver.global.api.dto.DetailIntroResponse;
+import ybe.mini.travelserver.global.api.dto.SearchKeywordResponse;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class TourAPIService {
-    // 숙박(accommodation) 객체에 매핑 후 db에 저장
-    private final AccommodationRepository accommodationRepository;
-    private final RoomRepository roomRepository;
 
-    private static final Random random = new Random();
+    public Accommodation bringAccommodation(
+            long accommodationId,
+            String keyword
+    ) {
+        var accommodationDetailResponse = TourAPIUtils.bringAccommodationDetail(accommodationId);
+        var accommodationsSimpleSearchResponse = TourAPIUtils.bringAccommodationByKeyword(keyword);
 
-    private void saveRoom(List<Long> accommodationIds) {
-        for (var accommodationId : accommodationIds) {
-            var roomItems = TourAPIUtils.bringRoom(accommodationId).response().body().items().item();
-            int randomPrice = (random.nextInt() * 45) * 10000 + 50000;
-            int randomStock = (random.nextInt() * 10) + 1;
-            List<Room> rooms = new ArrayList<>();
+        var detailItems = accommodationDetailResponse.response().body().items().item();
+        var keywordItems = accommodationsSimpleSearchResponse.response().body().items().item();
 
-            for (var roomItem : roomItems) {
-                rooms.add(
-                        Room.builder()
-                                .id(Long.parseLong(roomItem.roomcode()))
-                                .name(roomItem.roomtitle())
-                                .description(roomItem.roomintro())
-                                .price(randomPrice)
-                                .image(roomItem.roomimg1())
-                                .stock(randomStock)
-                                .capacity(Integer.parseInt(roomItem.roommaxcount()))
-                                .accommodation(Accommodation.builder().id(accommodationId).build())
-                                .build()
-                );
-            }
-
-            roomRepository.saveAll(rooms);
-        }
-    }
-
-    public void saveAccommodationsToDBFromFile() {
-        ObjectMapper mapper = new ObjectMapper();
-        List<Accommodation> accommodations = new ArrayList<>();
-
-        try {
-            JsonNode rootNode = mapper.readTree(new File("dummy/searchStay1.json"));
-            JsonNode responseNode = rootNode.get("response");
-            JsonNode bodyNode = responseNode.get("body");
-            JsonNode itemsNode = bodyNode.get("items");
-            JsonNode itemNode = itemsNode.get("item");
-
-            int cnt = 1;
-            if (itemNode.isArray()) {
-                for (JsonNode item : itemNode) {
-                    ObjectNode object = (ObjectNode) item;
-                    long contentId = object.get("contentid").asLong();
-
-                    Location location = Location.builder()
-                            .address(object.get("addr1").asText(""))
-                            .phone(object.get("tel").asText(""))
-                            .areaCode(object.get("areacode").asText(""))
-                            .latitude(object.get("mapy").asDouble())
-                            .longitude(object.get("mapx").asDouble())
-                            .build();
-                    accommodations.add(
-                            Accommodation.builder()
-                                    .id(contentId)
-                                    .name(item.get("title").asText(""))
-                                    .location(location)
-                                    .image(item.get("firstimage").asText(""))
-                                    .description(item.get("description").asText(""))
-                                    .build()
-                    );
-
-                    log.warn(cnt++ + "번째 숙소 저장 완료");
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("파일을 읽어오는데 실패했습니다.");
+        if (keywordItems.isEmpty() || detailItems.isEmpty()) {
+            throw new IllegalArgumentException("숙소 정보가 없습니다.");
         }
 
-        accommodationRepository.saveAll(accommodations);
+        var keywordItem = keywordItems.get(0);
+        var detailItem = detailItems.get(0);
+
+        return generateAccommodation(
+                accommodationId,
+                keywordItem,
+                detailItem
+        );
     }
 
+    public Accommodation bringAccommodation(String keyword) {
+        var accommodationsSimpleSearchResponse = TourAPIUtils.bringAccommodationByKeyword(keyword);
+
+        var keywordItems = accommodationsSimpleSearchResponse.response().body().items().item();
+
+        if (keywordItems.isEmpty()) {
+            throw new IllegalArgumentException("숙소 정보가 없습니다.");
+        }
+        var keywordItem = keywordItems.get(0);
+
+        long accommodationId = Long.parseLong(keywordItem.contentid());
+
+        var accommodationDetailResponse = TourAPIUtils.bringAccommodationDetail(accommodationId);
+        var detailItems = accommodationDetailResponse.response().body().items().item();
+        var detailItem = detailItems.get(0);
+
+        return generateAccommodation(
+                accommodationId,
+                keywordItem,
+                detailItem
+        );
+    }
+
+
+    public List<Room> bringRooms(long accommodationId) {
+        DetailInfoResponse detailInfoResponse = TourAPIUtils.bringRoom(accommodationId);
+
+        var roomItems = detailInfoResponse.response().body().items().item();
+
+        if (roomItems.isEmpty()) {
+            throw new IllegalArgumentException("객실 정보가 없습니다.");
+        }
+
+        List<Room> rooms = new ArrayList<>();
+        for (var roomItem : roomItems) {
+            Room room = Room.builder()
+                    .id(Long.valueOf(roomItem.contenttypeid()))
+                    .name(roomItem.roomtitle())
+                    .description(roomItem.roomintro())
+                    .price(Integer.parseInt(roomItem.roomoffseasonminfee1()))
+                    .capacity(Integer.parseInt(roomItem.roommaxcount()))
+                    .build();
+
+            rooms.add(room);
+        }
+
+        return rooms;
+    }
+
+    private static Accommodation generateAccommodation(
+            long accommodationId,
+            SearchKeywordResponse.Item keywordItem,
+            DetailIntroResponse.Item detailItem
+    ) {
+        return Accommodation.builder()
+                .id(accommodationId)
+                .name(keywordItem.title())
+                .image(keywordItem.firstimage())
+                .description(detailItem.subfacility())
+                .location(Location.builder()
+                        .address(keywordItem.addr1())
+                        .latitude(Double.valueOf(keywordItem.mapy()))
+                        .longitude(Double.valueOf(keywordItem.mapx()))
+                        .build())
+                .build();
+    }
 }
