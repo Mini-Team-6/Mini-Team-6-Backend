795b05ee153cc983d53208125063d10507c0c435
유현 <yh7119@gmail.com>
2023-11-25 18:02:00 +0900

    Fix: import 수정

--- src/main/java/ybe/mini/travelserver/domain/accommodation/controller/AccommodationController.java
+++ src/main/java/ybe/mini/travelserver/domain/accommodation/controller/AccommodationController.java
@@ -1,12 +1,9 @@
 package ybe.mini.travelserver.domain.accommodation.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ybe.mini.travelserver.domain.accommodation.dto.AccommodationAndRoomResponse;
 import ybe.mini.travelserver.domain.accommodation.dto.AccommodationGetResponse;
 import ybe.mini.travelserver.domain.accommodation.service.AccommodationService;
 import ybe.mini.travelserver.global.common.ResponseDto;
