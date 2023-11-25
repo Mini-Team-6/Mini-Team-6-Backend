16a52ec801dd1621c0c5357f4c52d32db8e85e2a
유현 <yh7119@gmail.com>
2023-11-25 17:59:15 +0900

    Feat: AreaCode Enum 추가

--- src/main/java/ybe/mini/travelserver/domain/accommodation/entity/AreaCode.java
+++ src/main/java/ybe/mini/travelserver/domain/accommodation/entity/AreaCode.java
@@ -6,24 +6,23 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum AreaCode {
-
-    SEOUL("1"),
-    INCHEON("2"),
-    DAEJEON("3"),
-    DAEGU("4"),
-    GWANGJU("5"),
-    BUSAN("6"),
-    ULSAN("7"),
-    SEJONG("8"),
-    GYEONGGI("31"),
-    GANGWON("32"),
-    CHUNGBUK("33"),
-    CHUNGNAM("34"),
-    GYEONGBUK("35"),
-    GYEONGNAM("36"),
-    JEONBUK("37"),
-    JEONNAM("38"),
-    JEJU("39");
+    SEOUL("서울", 1),
+    INCHEON("인천", 2),
+    DAEJEON("대전", 3),
+    DAEGU("대구", 4),
+    GWANGJU("광주", 5),
+    BUSAN("부산", 6),
+    ULSAN("울산", 7),
+    SEJONG("세종", 8),
+    GYEONGGI("경기", 31),
+    GANGWON("강원", 32),
+    CHUNGBUK("충북", 33),
+    CHUNGNAM("충남", 34),
+    GYEONGBUK("경북", 35),
+    GYEONGNAM("경남", 36),
+    JEONBUK("전북", 37),
+    JEONNAM("전남", 38),
+    JEJU("제주", 39);
 
     private final String city;
     private final int code;
