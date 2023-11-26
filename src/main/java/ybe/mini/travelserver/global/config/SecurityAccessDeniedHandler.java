5962233a26f6a7a269579c58045e47040d052271
okjaeook <okjaeook98@gmail.com>
2023-11-26 22:49:45 +0900

    Chore: 미사용 Exception 제거

--- src/main/java/ybe/mini/travelserver/global/config/SecurityAccessDeniedHandler.java
+++ src/main/java/ybe/mini/travelserver/global/config/SecurityAccessDeniedHandler.java
@@ -1,14 +1,11 @@
 package ybe.mini.travelserver.global.config;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-
-import java.io.IOException;
 
 @Component
 public class SecurityAccessDeniedHandler implements AccessDeniedHandler {
@@ -23,7 +20,7 @@ public class SecurityAccessDeniedHandler implements AccessDeniedHandler {
             HttpServletRequest request,
             HttpServletResponse response,
             AccessDeniedException accessDeniedException
-    ) throws IOException, ServletException {
+    ) {
         handlerExceptionResolver.resolveException(
                 request,
                 response,
--- src/main/java/ybe/mini/travelserver/global/config/SecurityAuthenticationEntryPoint.java
+++ src/main/java/ybe/mini/travelserver/global/config/SecurityAuthenticationEntryPoint.java
@@ -1,14 +1,11 @@
 package ybe.mini.travelserver.global.config;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-
-import java.io.IOException;
 
 @Component
 public class SecurityAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -24,7 +21,7 @@ public class SecurityAuthenticationEntryPoint implements AuthenticationEntryPoin
             HttpServletRequest request,
             HttpServletResponse response,
             AuthenticationException authException
-    ) throws IOException, ServletException {
+    ) {
         handlerExceptionResolver.resolveException(
                 request,
                 response,
