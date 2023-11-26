9e090881f7345cedded103c1fe1e9e18f7523d6f
okjaeook <okjaeook98@gmail.com>
2023-11-26 22:45:20 +0900

    Refactor: Security 체계 개편
    `#` 화이트리스트 -> 블랙리스트 페이지 제어
    `#` 페이지 인증, 인가를 controller 메서드 단위에서 제어
    `#` 오류는 RestControllerAdvice 에 수집됨

---
+++ src/main/java/ybe/mini/travelserver/global/config/SecurityAccessDeniedHandler.java
@@ -0,0 +1,34 @@
+package ybe.mini.travelserver.global.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+@Component
+public class SecurityAccessDeniedHandler implements AccessDeniedHandler {
+    HandlerExceptionResolver handlerExceptionResolver;
+
+    public SecurityAccessDeniedHandler(HandlerExceptionResolver handlerExceptionResolver) {
+        this.handlerExceptionResolver = handlerExceptionResolver;
+    }
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException
+    ) throws IOException, ServletException {
+        handlerExceptionResolver.resolveException(
+                request,
+                response,
+                null,
+                accessDeniedException
+        );
+    }
+}
