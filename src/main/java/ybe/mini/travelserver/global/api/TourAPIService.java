2aae5ca8ef15334b590c24cc34d249d445f9536f
유현 <yh7119@gmail.com>
2023-11-28 03:35:52 +0900

    Fix: api 로직 수정

--- src/main/java/ybe/mini/travelserver/global/api/TourAPIService.java
+++ src/main/java/ybe/mini/travelserver/global/api/TourAPIService.java
@@ -6,85 +6,120 @@ import org.springframework.stereotype.Service;
 import ybe.mini.travelserver.domain.accommodation.entity.Accommodation;
 import ybe.mini.travelserver.domain.accommodation.entity.Location;
 import ybe.mini.travelserver.domain.room.entity.Room;
-import ybe.mini.travelserver.global.api.dto.DetailInfoResponse;
-import ybe.mini.travelserver.global.api.dto.DetailIntroResponse;
-import ybe.mini.travelserver.global.api.dto.SearchKeywordResponse;
+import ybe.mini.travelserver.global.api.dto.AccommodationTourAPIResponse;
+import ybe.mini.travelserver.global.api.dto.RoomTourAPIResponse;
+import ybe.mini.travelserver.global.exception.api.NoAccommodationsFromAPIException;
+import ybe.mini.travelserver.global.exception.api.NoRoomsFromAPIException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class TourAPIService {
-
     public Accommodation bringAccommodation(
-            long accommodationId,
-            String keyword
+            String keyword,
+            String code
     ) {
-        var accommodationDetailResponse = TourAPIUtils.bringAccommodationDetail(accommodationId);
-        var accommodationsSimpleSearchResponse = TourAPIUtils.bringAccommodations(keyword);
+        var accommodationsSimpleSearchResponse = TourAPIUtils.bringAccommodation(keyword, code);
 
-        var detailItems = accommodationDetailResponse.response().body().items().item();
-        var keywordItems = accommodationsSimpleSearchResponse.response().body().items().item();
+        var body = accommodationsSimpleSearchResponse.response().body();
 
-        if (keywordItems.isEmpty() || detailItems.isEmpty()) {
-            throw new IllegalArgumentException("숙소 정보가 없습니다.");
+        if (body.totalCount() == 0) {
+            throw new NoAccommodationsFromAPIException();
         }
 
-        var keywordItem = keywordItems.get(0);
-        var detailItem = detailItems.get(0);
+        var items = body.items().item();
+
+
+        var item = items.get(0);
+
+        long accommodationId = Long.parseLong(item.contentid());
 
         return generateAccommodation(
                 accommodationId,
-                keywordItem,
-                detailItem
+                item
         );
     }
 
-    public Accommodation bringAccommodation(String keyword) {
-        var accommodationsSimpleSearchResponse = TourAPIUtils.bringAccommodations(keyword);
 
-        var keywordItems = accommodationsSimpleSearchResponse.response().body().items().item();
-
-        if (keywordItems.isEmpty()) {
-            throw new IllegalArgumentException("숙소 정보가 없습니다.");
-        }
-        var keywordItem = keywordItems.get(0);
-
-        long accommodationId = Long.parseLong(keywordItem.contentid());
-
-        var accommodationDetailResponse = TourAPIUtils.bringAccommodationDetail(accommodationId);
-        var detailItems = accommodationDetailResponse.response().body().items().item();
-        var detailItem = detailItems.get(0);
-
-        return generateAccommodation(
-                accommodationId,
-                keywordItem,
-                detailItem
+    public List<Accommodation> bringAccommodations(
+            int pageNo,
+            int numOfRows,
+            String keyword,
+            String code
+    ) {
+        var accommodationsSimpleSearchResponse = TourAPIUtils.bringAccommodations(
+                pageNo,
+                numOfRows,
+                keyword,
+                code
         );
+
+        var body = accommodationsSimpleSearchResponse.response().body();
+
+        if (body.totalCount() == 0) {
+            throw new NoAccommodationsFromAPIException();
+        }
+        var items = body.items().item();
+
+        List<Accommodation> accommodations = new ArrayList<>();
+
+        for (var item : items) {
+            long accommodationId = Long.parseLong(item.contentid());
+
+            accommodations.add(
+                    generateAccommodation(
+                            accommodationId,
+                            item
+                    )
+            );
+        }
+
+        return accommodations;
+    }
+
+    private static Accommodation generateAccommodation(
+            long accommodationId,
+            AccommodationTourAPIResponse.Item keywordItem
+    ) {
+        return Accommodation.builder()
+                .id(accommodationId)
+                .name(keywordItem.title())
+                .image(keywordItem.firstimage())
+                .location(Location.builder()
+                        .address(keywordItem.addr1())
+                        .latitude(Double.valueOf(keywordItem.mapy()))
+                        .longitude(Double.valueOf(keywordItem.mapx()))
+                        .phone(keywordItem.tel())
+                        .areaCode(keywordItem.areacode())
+                        .build())
+                .build();
     }
 
     public List<Room> bringRooms(long accommodationId) {
-        DetailInfoResponse detailInfoResponse = TourAPIUtils.bringRoom(accommodationId);
+        RoomTourAPIResponse roomTourAPIResponse = TourAPIUtils.bringRoom(accommodationId);
 
-        var roomItems = detailInfoResponse.response().body().items().item();
+        var body = roomTourAPIResponse.response().body();
 
-        if (roomItems.isEmpty()) {
-            throw new IllegalArgumentException("객실 정보가 없습니다.");
+        if (body.totalCount() == 0) {
+            throw new NoRoomsFromAPIException();
         }
 
+        var items = body.items().item();
+
+
         List<Room> rooms = new ArrayList<>();
-        for (var roomItem : roomItems) {
+        for (var item : items) {
             Room room = Room.builder()
-                    .roomTypeId(Long.valueOf(roomItem.roomcode()))
-                    .name(roomItem.roomtitle())
-                    .description(roomItem.roomintro())
-                    .price(Integer.parseInt(roomItem.roomoffseasonminfee1()))
-                    .image(roomItem.roomimg1())
-                    .stock(Integer.parseInt(roomItem.roomcount()))
-                    .capacity(Integer.parseInt(roomItem.roommaxcount()))
+                    .roomTypeId(Long.valueOf(item.roomcode()))
+                    .name(item.roomtitle())
+                    .description(item.roomintro())
+                    .price(Integer.parseInt(item.roomoffseasonminfee1()))
+                    .image(item.roomimg1())
+                    .stock(Integer.parseInt(item.roomcount()))
+                    .capacity(Integer.parseInt(item.roommaxcount()))
                     .accommodation(
                             Accommodation.builder()
                                     .id(accommodationId)
@@ -110,66 +145,6 @@ public class TourAPIService {
             }
         }
 
-        throw new IllegalArgumentException("객실 정보가 없습니다.");
-    }
-
-    public List<Accommodation> bringAccommodationsForSearch(
-            int pageNo,
-            int numOfRows,
-            String keyword,
-            String areaCode
-    ) {
-        var accommodationsSimpleSearchResponse = TourAPIUtils.bringAccommodations(
-                pageNo,
-                numOfRows,
-                keyword,
-                areaCode
-        );
-
-        var searchedItems = accommodationsSimpleSearchResponse.response().body().items().item();
-
-        if (searchedItems.isEmpty()) {
-            throw new IllegalArgumentException("숙소 정보가 없습니다.");
-        }
-
-        List<Accommodation> accommodations = new ArrayList<>();
-
-        for (var item : searchedItems) {
-            long accommodationId = Long.parseLong(item.contentid());
-
-            accommodations.add(
-                    generateAccommodation(
-                            accommodationId,
-                            item,
-                            null
-                    )
-            );
-        }
-
-        return accommodations;
-    }
-
-    private static Accommodation generateAccommodation(
-            long accommodationId,
-            SearchKeywordResponse.Item keywordItem,
-            DetailIntroResponse.Item detailItem
-    ) {
-        String subfacility = Optional.ofNullable(detailItem)
-                .map(DetailIntroResponse.Item::subfacility)
-                .orElse("");
-
-        return Accommodation.builder()
-                .id(accommodationId)
-                .name(keywordItem.title())
-                .image(keywordItem.firstimage())
-                .description(subfacility)
-                .location(Location.builder()
-                        .address(keywordItem.addr1())
-                        .latitude(Double.valueOf(keywordItem.mapy()))
-                        .longitude(Double.valueOf(keywordItem.mapx()))
-                        .phone(keywordItem.tel())
-                        .areaCode(keywordItem.areacode())
-                        .build())
-                .build();
+        throw new NoRoomsFromAPIException();
     }
 }
