5a5fb793886239c1744b7fc3ed83963008e2178e
tjdtn0219 <s_sjang@naver.com>
2023-11-27 18:58:19 +0900

    Fix: ReservationRoomController hasRole 추가

--- src/main/java/ybe/mini/travelserver/domain/reservation_room/controller/ReservationRoomController.java
+++ src/main/java/ybe/mini/travelserver/domain/reservation_room/controller/ReservationRoomController.java
@@ -3,6 +3,7 @@ package ybe.mini.travelserver.domain.reservation_room.controller;//package ybe.m
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ybe.mini.travelserver.domain.reservation_room.dto.ReservationRoomGetResponse;
 import ybe.mini.travelserver.domain.reservation_room.service.ReservationRoomService;
@@ -18,6 +19,7 @@ public class ReservationRoomController {
 
     private final ReservationRoomService reservationRoomService;
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/{reservationId}")
     public ResponseDto<List<ReservationRoomGetResponse>> getReservationRoomsByReserveId(
             @PathVariable Long reservationId
@@ -28,7 +30,8 @@ public class ReservationRoomController {
                 reservationRoomService.getReservationRoomsFromReservation(reservationId)
         );
     }
-
+    
+    @PreAuthorize("hasRole('ROLE_USER')")
     @DeleteMapping
     public ResponseDto<Long> deleteReservationRoom(
             @RequestParam Long reservationId,
