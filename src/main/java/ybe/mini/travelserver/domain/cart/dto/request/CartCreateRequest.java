4886c2abf395fa8129322012aae3bd4564783d57
minjung <ypd06021@naver.com>
2023-11-28 19:23:18 +0900

    Fix: reservation 테스트 오류 수정

--- src/main/java/ybe/mini/travelserver/domain/cart/dto/request/CartCreateRequest.java
+++ src/main/java/ybe/mini/travelserver/domain/cart/dto/request/CartCreateRequest.java
@@ -1,12 +1,12 @@
 package ybe.mini.travelserver.domain.cart.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import ybe.mini.travelserver.domain.accommodation.entity.AreaCode;
-
-import java.time.LocalDate;
 
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
