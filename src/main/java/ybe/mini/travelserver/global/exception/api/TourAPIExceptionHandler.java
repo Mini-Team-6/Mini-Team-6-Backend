170f3cca3e98cc5fba161aa418411950df587811
okjaeook <okjaeook98@gmail.com>
2023-11-28 18:27:40 +0900

    Feat: TourAPI error handler 추가

--- src/main/java/ybe/mini/travelserver/global/exception/api/TourAPIExceptionHandler.java
+++ src/main/java/ybe/mini/travelserver/global/exception/api/TourAPIExceptionHandler.java
@@ -1,10 +1,14 @@
 package ybe.mini.travelserver.global.exception.api;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ybe.mini.travelserver.global.exception.ProblemDetailCreator;
+
+import static ybe.mini.travelserver.global.exception.api.TourAPIErrorMessage.*;
+
 
 @RestControllerAdvice
 public class TourAPIExceptionHandler extends ProblemDetailCreator<TourAPIErrorMessage> {
@@ -14,12 +18,23 @@ public class TourAPIExceptionHandler extends ProblemDetailCreator<TourAPIErrorMe
 
     @ExceptionHandler(NoAccommodationsFromAPIException.class)
     public ProblemDetail handleNotGatheredAccommodationsFromAPIException(HttpServletRequest request) {
-        return createProblemDetail(TourAPIErrorMessage.NO_ACCOMMODATIONS_FROM_API, request);
+        return createProblemDetail(NO_ACCOMMODATIONS_FROM_API, request);
     }
 
     @ExceptionHandler(NoRoomsFromAPIException.class)
     public ProblemDetail handleNotGatheredRoomsFromAPIException(HttpServletRequest request) {
-        return createProblemDetail(TourAPIErrorMessage.NO_ROOMS_FROM_API, request);
+        return createProblemDetail(NO_ROOMS_FROM_API, request);
     }
 
+    @ExceptionHandler(WrongXMLFormatException.class)
+    public ProblemDetail handleWrongXMLFormatException(
+            WrongXMLFormatException ex,
+            HttpServletRequest request) {
+        return createProblemDetail(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(), request);
+    }
+
+    @ExceptionHandler(WrongRequestException.class)
+    public ProblemDetail handleWrongCallBackException(WrongRequestException ex, HttpServletRequest request) {
+        return createProblemDetail(valueOf(ex.getMessage()), request);
+    }
 }
