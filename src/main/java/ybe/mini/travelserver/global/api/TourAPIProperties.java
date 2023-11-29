941379491a9f3f56d70a9b3dc9aa1f01d6aa8c11
Ok JaeOok <okjaeook98@gmail.com>
2023-11-29 15:56:26 +0900

    Chore: API 키 갱신

--- src/main/java/ybe/mini/travelserver/global/api/TourAPIProperties.java
+++ src/main/java/ybe/mini/travelserver/global/api/TourAPIProperties.java
@@ -5,8 +5,8 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TourAPIProperties {
-    public static final String KEY_DECODED = "rKtD0jUO3f83Q/Q7QJ7yxo2GMBF489d4y5NM3H48uUffmUXjeoARqF4wmsICTBt3BVS988w8vG4Nh2UZtMV2/A==";
-    public static final String KEY_ENCODED = "rKtD0jUO3f83Q%2FQ7QJ7yxo2GMBF489d4y5NM3H48uUffmUXjeoARqF4wmsICTBt3BVS988w8vG4Nh2UZtMV2%2FA%3D%3D";
+    public static final String KEY_DECODED = "0ON3kBtDxd0KYGW5spO8inNljADd/IqfnS/l3nMWq3EkARl20N3MmZVYlSvH3Y8V5fEAo7Seucd5pR7Ebm2Phg==";
+    public static final String KEY_ENCODED = "0ON3kBtDxd0KYGW5spO8inNljADd%2FIqfnS%2Fl3nMWq3EkARl20N3MmZVYlSvH3Y8V5fEAo7Seucd5pR7Ebm2Phg%3D%3D";
     public static final String BASE_URL = "https://apis.data.go.kr/B551011/KorService1/";
 
     public static final String SEARCH_KEYWORD = "searchKeyword1";
