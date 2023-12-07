4819ecd8e5d41a591d0268965c3a24315b40ee16
Ok JaeOok <okjaeook98@gmail.com>
2023-12-07 19:03:35 +0900

    Feat: refresh 토큰 구현
    `#` access 토큰 내 payload 개인정보 최소화
    `+` redis로 refresh 구현
    `-` JwtDecoder, JwtIssuer -> JwtService

--- src/main/java/ybe/mini/travelserver/global/security/JwtAuthenticationFilter.java
+++ src/main/java/ybe/mini/travelserver/global/security/JwtAuthenticationFilter.java
@@ -1,25 +1,25 @@
 package ybe.mini.travelserver.global.security;
 
 import com.auth0.jwt.exceptions.*;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private final JwtDecoder jwtDecoder;
+    private final JwtService jwtService;
     private final JwtToPrincipalConverter jwtToPrincipalConverter;
     private final HandlerExceptionResolver handlerExceptionResolver;
 
@@ -29,39 +29,58 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+        String accessToken = jwtService.extractTokenFromRequest(request);
+
+        if (accessToken == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        DecodedJWT decodedJWT = jwtService.decode(accessToken);
 
         try {
-            extractTokenFromRequest(request)
-                    .map(jwtDecoder::decode)
-                    .map(jwtToPrincipalConverter::convert)
-                    .map(UserPrincipalAuthenticationToken::new)
-                    .ifPresent(authentication -> SecurityContextHolder.getContext().setAuthentication(authentication));
+            decodedJWT = jwtService.verify(decodedJWT);
+            signAuthentication(decodedJWT);
+        } catch (TokenExpiredException ex) {
+            handleTokenExpired(decodedJWT, response);
         } catch (
                 SignatureGenerationException |
                 AlgorithmMismatchException |
                 InvalidClaimException |
                 JWTDecodeException |
-                SignatureVerificationException |
-                TokenExpiredException ex
+                SignatureVerificationException ex
         ) {
-            handlerExceptionResolver.resolveException(
-                    request,
-                    response,
-                    null,
-                    ex
-            );
-
+            handlerExceptionResolver.resolveException(request, response, null, ex);
             return;
         }
 
         filterChain.doFilter(request, response);
     }
 
-    private Optional<String> extractTokenFromRequest(HttpServletRequest request) {
-        var token = request.getHeader("Authorization");
-        if (StringUtils.hasText(token) && token.startsWith("Bearer ")) {
-            return Optional.of(token.substring(7));
+    private void handleTokenExpired(DecodedJWT decodedJWT, HttpServletResponse response) {
+        String refreshToken = jwtService.getRefreshToken(decodedJWT.getId());
+
+        if (refreshToken != null) {
+            DecodedJWT decodedRefreshToken = jwtService.decode(refreshToken);
+            String newAccessToken = jwtService.createAccessToken(
+                    decodedRefreshToken.getClaim("email").asString(),
+                    decodedRefreshToken.getId()
+            );
+            response.setHeader("Authorization", "Bearer " + newAccessToken);
+            signAuthentication(decodedRefreshToken);
+        } else {
+            SecurityContextHolder.clearContext();
         }
-        return Optional.empty();
+    }
+
+    private void signAuthentication(DecodedJWT decodedJWT) {
+        PrincipalDetails principalDetails = jwtToPrincipalConverter.convert(decodedJWT);
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(
+                        principalDetails,
+                        null,
+                        principalDetails.getAuthorities()
+                )
+        );
     }
 }
